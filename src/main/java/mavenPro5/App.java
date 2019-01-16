f1ad02b64ebc7137e322548cc49138badfc3abea
liuhuifang <652170554@qq.com>
2019-01-16 21:03:38 +0800

    V1，Created!

---
+++ src/main/java/mavenPro5/App.java
@@ -0,0 +1,13 @@
+package mavenPro5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
