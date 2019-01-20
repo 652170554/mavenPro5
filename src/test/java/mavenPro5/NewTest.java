4046861a71a1f2fc270eaf5deb3c519f3f5e95a6
liuhuifang <652170554@qq.com>
2019-01-20 22:01:36 +0800

    qq

--- src/test/java/mavenPro5/NewTest.java
+++ src/test/java/mavenPro5/NewTest.java
@@ -1,13 +1,16 @@
 package mavenPro5;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+import static org.testng.Assert.*;
 public class NewTest {
   @Test
   public void f() {
-System.out.println("5");  }
+      System.out.println("5"); 
+      assertTrue(false);    
+  }
 
   @BeforeMethod
   public void beforeMethod() {
